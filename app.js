f50825573b7ad43cb1a5528fa5f66a2213a42db2
Tupap1 <126369055+Tupap1@users.noreply.github.com>
2024-04-17 10:56:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-document.getElementById("Info").innerHTML=''
 
-function buscar(){
+function buscar() {
 
-    
-alert("ya recibimos tu consulta")
+  document.getElementById("resultado").innerHTML = ''
+  asd = document.getElementById("Info").value
 
-const requestOptions = {
+  alert("Recibimos tu consulta")
+
+  const requestOptions = {
     method: "GET",
     redirect: "follow"
   };
-  
 
-  let url = "http://ergast.com/api/f1/drivers" + 
+
+  let url = "http://ergast.com/api/f1/drivers/" + asd + ".json"
 
   fetch(url)
     .then((response) => response.json())
     .then((result) => console.log(result))
-    .catch((error) => console.error(error));  
+    .catch((error) => console.error(error));
+
+
+
+  mostrarDatos()
+
+
+
+}
+
+function mostrarDatos(data) {
+  const mostrarDatos1 = (data => {
+    console.log(Drivers)
+
+    infoPiloto = ` ${MRData.DriverTable.Drivers[0]}`
+
+    document.getElementById("resultado").innerHTML = infoPiloto
   }
+  )
+}
